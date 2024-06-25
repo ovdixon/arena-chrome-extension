378ddfa38cb632d725c8c2ececc29741670dc5fa
Oliver Dixon <ovdixon@icloud.com>
2024-06-25 15:26:43 +1000

    My first commit

--- README.md
+++
@@ -1,7 +0,0 @@
-## Add to Are.na / Chrome Extension
-
-<img src="/img/preview.png" width="1000">
-
-### Contributing
-
-PR's welcome! See [CONTRIBUTING](CONTRIBUTING.md).
--- js/background.js
+++ js/background.js
@@ -1,26 +1,51 @@
-// context menu
-chrome.contextMenus.create({
-  title: "Add to Are.na",
-  contexts: ["page","selection","link","editable","image","video", "audio"],
-  onclick: function(options){
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {
-        text: "open:dialog",
-        options: options,
-        title: tabs[0].title,
-        url: tabs[0].url,
-        tab_id: tabs[0].id
-      }, function(content){});
+let screenshotUrl = null;
+let window;
+
+chrome.sidePanel
+  .setPanelBehavior({ openPanelOnActionClick: true })
+  .catch((error) => console.error(error));
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+        id: "arenaSnip",
+        title: "Snip to Are.na"
     });
-  }
 });
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.sendMessage(tab.id, {
-    text: "open:dialog",
-    options: { srcUrl: tab.url },
-    title: tab.title,
-    url: tab.url,
-    tab_id: tab.id
-  }, function(content){});
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId === "arenaSnip") {
+        window = tab.windowId
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            files: ["scripts/content-script.js"]
+        });
+    }
+});
+
+chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
+    if (req.message === "capture") {
+        chrome.tabs.captureVisibleTab(null, { format: 'jpeg', quality: 100 }, function (dataUrl) {
+            sendResponse({ imgSrc: dataUrl });
+        });
+        chrome.sidePanel.open({windowId: window});
+
+    }
+    return true;
+});
+
+chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
+    if (req.message === "crop") {
+        screenshotUrl = req.img
+    }
+    return true;
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message === "requestScreenshot") {
+        if (screenshotUrl) {
+            sendResponse(screenshotUrl);
+        } else {
+            sendResponse(null);
+        }
+    }
 });
